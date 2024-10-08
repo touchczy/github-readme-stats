5cc3bfef0e76e3cd5b12bd72e652e2943896b8f3
Czy <79649307+touchczy@users.noreply.github.com>
2024-10-08 09:06:46 +0800

    chore: debugger dev.js

--- dev.js
+++ dev.js
@@ -14,4 +14,5 @@ app.get("/pin", repoCard);
 app.get("/top-langs", langCard);
 app.get("/wakatime", wakatimeCard);
 
+// http://localhost:9000/api?icon_color=333&hide_border=true&title_color=333&username=WindrunnerMax&show_icons=true&include_all_commits=true
 console.log("http://localhost:9000/");
